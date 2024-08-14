67dccea6509518119a138c44932de6b40cc63cac
Mohamedbenbelkhir <Mohamedbenbelkhir1000@gmail.com>
2024-08-14 12:23:06 +0100

    add create and update delete for the invoices add login accessibilty for the aplication add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,6 +3,11 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import CardWrapper from "@/app/ui/dashboard/cards";
+
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "dashboard ",
+};
 // import { fetchRevenue } from "../lib/data";
 // import {
 // fetchRevenue,
